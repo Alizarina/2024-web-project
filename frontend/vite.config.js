43a3762051de04fc7cd009bfe3a2835b4409e11b
231250180 <231250180@smail.nju.edu.cn>
2024-08-13 22:54:14 +0800

    完成版本

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
+  assetsDir: 'static',
+  parallel: false,
+  publicPath: './',
   plugins: [react()],
   server: {
     proxy: {
