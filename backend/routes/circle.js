32af798b31eface406e114d748f1afe50dc33e32
231250180 <231250180@smail.nju.edu.cn>
2024-08-13 22:01:41 +0800

    最后一个bug

--- backend/routes/circle.js
+++ backend/routes/circle.js
@@ -21,5 +21,60 @@ router.get('/', function(req, res, next) {
   })
   connection.end();
 });
-
+router.post('/details', function(req, res, next) {
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'1234',
+    database:'user_data',
+    port:3306
+  })
+  let name=req.body.name;
+  const sql=`SELECT circle_list.description FROM circle_list WHERE name='${name}'`;
+  connection.query(sql, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.send(result[0].description);
+  })
+});
+router.post('/blogs', function(req, res, next) {
+  let name=req.body.name;
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'1234',
+    database:'user_data',
+    port:3306
+  })
+  const sql=`SELECT * FROM ${name}`;
+  connection.connect();
+  connection.query(sql, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.send(result);
+  })
+  connection.end();
+});
+router.post('/huoyue', function(req, res){
+  let name=req.body.name;
+  let base=name+'_user'
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'1234',
+    database:'user_data',
+    port:3306
+  })
+  const sql=`SELECT * FROM ${base}`;
+  connection.connect();
+  connection.query(sql, (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.send(result);
+  })
+  connection.end();
+});
 module.exports = router;
--- backend/routes/create.js
+++ backend/routes/create.js
@@ -62,4 +62,187 @@ router.post('/', function(req, res) {
     })
     connection.end();
 });
+router.post('/huoyue', function(req, res){
+    let userbody=req.body;
+    let username=userbody.username;
+    let circle=userbody.circle;
+    let user_table=circle+'_user';
+    const sql=`SELECT * FROM ${user_table} WHERE username='${username}'`;
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    connection.connect();
+    connection.query(sql, (error, result) => {
+        IfAllow=0;
+        if (error) {
+            throw error;
+        }
+        if(IfAllow===0){
+            if (result.length > 0) {
+                res.send({id:1})//存在
+            }else{
+                res.send({id:0})//不存在
+            }
+        }
+    })
+    connection.end();
+});
+router.post('/jiaruhuoyue', function(req, res){
+    let userbody=req.body;
+    let username=userbody.username;
+    let circle=userbody.circle;
+    let user_table=circle+'_user';
+    const sql=`INSERT INTO ${user_table} (username,huoyuedu) VALUES ('${username}', 3);`;
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    connection.connect();
+    connection.query(sql, (error, result) => {
+        if (error){
+            throw error;
+        }
+        res.send({finish:1});
+    })
+    connection.end();
+});
+router.post('/blog', function(req, res){
+    let userbody=req.body;
+    let username=userbody.username;
+    let circle=userbody.circle;
+    let title=userbody.title;
+    let about=userbody.about;
+    let pic=userbody.pic;
+    const sql=`INSERT INTO ${circle} (blog_id,author_name,content,pic1,pic2) VALUES (0,'${username}','${about}','${title}','${pic}');`;
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    connection.connect();
+    connection.query(sql, (error, result) => {
+        if (error){
+            throw error;
+        }
+        res.send({finish:1});
+    })
+    connection.end();
+});
+router.post('/jingyanzhi', function(req, res){
+    let userbody=req.body;
+    let username=userbody.username;
+    let circle=userbody.circle;
+    let user_table=circle+'_user';
+    const sql=`UPDATE ${user_table} SET huoyuedu = huoyuedu + 3 WHERE username = '${username}';`;
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    connection.connect();
+    connection.query(sql, (error, result) => {
+        if (error){
+            throw error;
+        }
+        res.send({finish:1});
+    })
+    connection.end();
+});
+router.post('/jingyanzhimini', function(req, res){
+    let userbody=req.body;
+    let username=userbody.username;
+    let circle=userbody.circle;
+    let user_table=circle+'_user';
+    const sql=`UPDATE ${user_table} SET huoyuedu = huoyuedu + 1 WHERE username = '${username}';`;
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    connection.connect();
+    connection.query(sql, (error, result) => {
+        if (error){
+            throw error;
+        }
+        res.send({finish:1});
+    })
+    connection.end();
+});
+router.post('/checkpinglun', function(req, res){
+    let userbody=req.body;
+    let number=userbody.number;
+    let circle=userbody.circle;
+    let connection= mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    const sql=`SELECT * FROM ${circle} WHERE blog_id=${number}`;
+    connection.connect();
+    connection.query(sql, (error, result) => {
+        if (error) {
+            throw error;
+        }
+        res.send(result);
+    });
+    connection.end();
+});
+router.post('/pinglun', function(req, res){
+    let userbody=req.body;
+    let number=userbody.number;
+    let circle=userbody.circle;
+    let count=userbody.count;
+    let username=userbody.username;
+    let about=userbody.about;
+    let str=username+': '+about;
+    let connection= mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password:'1234',
+        database:'user_data',
+        port:3306
+    })
+    const sql1=`UPDATE ${circle} SET com1 = '${str}' WHERE blog_id=${number} `;
+    const sql2=`UPDATE ${circle} SET com2 = '${str}' WHERE blog_id=${number} `;
+    const sql3=`UPDATE ${circle} SET com3 = '${str}' WHERE blog_id=${number} `;
+    connection.connect();
+    if(count===0){
+        connection.query(sql1, (error, result) => {
+            if (error) {
+                throw error;
+            }
+            res.send(result);
+        });
+    }else if(count===1){
+        connection.query(sql2, (error, result) => {
+            if (error) {
+                throw error;
+            }
+            res.send(result);
+        });
+    }else if(count===2){
+        connection.query(sql3, (error, result) => {
+            if (error) {
+                throw error;
+            }
+            res.send(result);
+        });
+    }
+    connection.end();
+});
 module.exports = router;
